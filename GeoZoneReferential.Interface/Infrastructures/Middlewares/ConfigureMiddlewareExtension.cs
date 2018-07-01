13a82d90d0a2838ef669984bdfaca397c6bf05b8
Blennouill <k.blenner4@gmail.com>
2018-07-01 15:27:51 +0200

    hangfire init

---
+++ GeoZoneReferential.Interface/Infrastructures/Middlewares/ConfigureMiddlewareExtension.cs
@@ -0,0 +1,21 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+
+namespace GeoZoneReferential.Interface.Infrastructures.Middlewares
+{
+    /// <summary>
+    /// Is used to regroup middleware declaration in the configuration pipeline.
+    /// </summary>
+    public static class ConfigureMiddlewareExtension
+    {
+        /// <summary>
+        /// Enable the custom hangfire configuration in the pipeline.
+        /// </summary>
+        /// <param name="app"><see cref="IApplicationBuilder"/></param>
+        public static IApplicationBuilder UseCustomHangfire(this IApplicationBuilder app)
+        {
+            return app  .UseHangfireDashboard()
+                        .UseHangfireServer();
+        }
+    }
+}
